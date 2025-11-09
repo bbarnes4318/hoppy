6a122080c99e94460cb70d986ebc657b5fcc3bc7
Braden Barnes <bbarnes4318@gmail.com>
2025-11-08 22:43:53 -0500

    Fix webpack alias config with proper path resolution

--- web/next.config.js
+++ web/next.config.js
@@ -6,11 +6,15 @@ const nextConfig = {
   images: {
     domains: [],
   },
-  webpack: (config) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
+      '@': path.resolve(__dirname, './'),
     };
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, './'),
+    ];
     return config;
   },
   async rewrites() {
